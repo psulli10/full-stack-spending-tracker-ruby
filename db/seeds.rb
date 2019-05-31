18a1c0dab09df38d0c1531e10b39df2119395151
psulli10 <petersullivan1985@gmail.com>
2019-05-31 11:01:38 +0100

    Tag class set up

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,9 +4,11 @@ require_relative( '../models/transaction.rb' )
 require('pry')
 
 Merchant.delete_all()
-# Tag.delete_all()
+Tag.delete_all()
 # Transaction.delete_all()
 
+
+# MERCHANTS
 merchant1 = Merchant.new({
   'name' => 'Scotrail'
 })
@@ -22,6 +24,24 @@ merchant1.name = 'CodeClan'
 merchant1.update()
 
 all_merchants = Merchant.all()
+
+
+# TAGS
+tag1 = Tag.new({
+  'name' => 'Travel'
+})
+
+tag2 = Tag.new({
+  'name' => 'Beer'
+})
+
+tag1.save()
+tag2.save()
+
+tag1.name = 'Groceries'
+tag1.update()
+
+all_tags = Tag.all()
 
 binding.pry
 
---
+++ models/tag.rb
@@ -0,0 +1,52 @@
+require_relative( '../db/sql_runner' )
+
+class Tag
+
+  attr_accessor :name
+  attr_reader :id
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+  end
+
+  def save()
+    sql = "INSERT INTO tags (name) VALUES ($1) RETURNING id"
+    values = [@name]
+    results = SqlRunner.run(sql, values)
+    @id = results.first()['id'].to_i
+  end
+
+  def delete()
+    sql = "DELETE from tags WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def find()
+    sql = "SELECT * FROM tags WHERE id = $1"
+    values = [@id]
+    result = SqlRunner.run(sql, values)[0]
+    return Tag.new(result)
+  end
+
+  def update()
+    sql = "UPDATE tags SET name = $1 WHERE id = $2"
+    values = [@name, @id]
+    SqlRunner.run(sql, values)
+  end
+
+
+  def self.all()
+    sql = "SELECT * FROM tags"
+    results = SqlRunner.run(sql)
+    return results.map{|tag| Tag.new(tag)}
+  end
+
+  def self.delete_all()
+    sql = "DELETE from tags"
+    SqlRunner.run(sql)
+  end
+
+
+end
